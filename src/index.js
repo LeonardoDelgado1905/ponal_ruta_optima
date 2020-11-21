2107c7e75c30248b2713b2370a5e2876b6175ba3
Leonardo Delgado <sdelgadom@unal.edu.co>
2020-11-21 11:57:33 -0500

    mapa en Berlin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
+const router = express.Router();
 app.set('port', process.env.PORT || 3000)
 
-app.get('/', (req, res) => {
-res.send('Hello aaaWorld!')
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-
+app.use(router)
 app.listen(app.get('port'), () => {
     console.log("esta corriendo weeey")
 })
