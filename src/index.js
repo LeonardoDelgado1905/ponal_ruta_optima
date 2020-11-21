f3b20b7c0d61781d59207fe7b594cc06cc7ba1eb
Leonardo Delgado <sdelgadom@unal.edu.co>
2020-11-21 16:58:51 -0500

    Decoded polyline

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
 
-const express = require('express')
+// const express = require('express')
+import express from 'express'
 const app = express()
-const path = require('path')
+// const path = require('path')
+import path from 'path'
 const router = express.Router()
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+import fetch from 'node-fetch'
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 app.set('port', process.env.PORT || 3000)
+// const polyline = require('@mapbox/polyline');
+
+import {decode} from './hereDecode.js';
+
 
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-router.get('/getRoute', function(req, res){
+
+function printRuta(data){
+    var lat = data.routes[0].sections[0].departure.place.location.lat
+    var lng = data.routes[0].sections[0].departure.place.location.lng
+    var lineString = data.routes[0].sections[0].polyline
+
+    // lineString.coordinates.forEach(coor =>{
+    //     lat += coor[1]
+    //     console.log(lat)
+    // } )
     
+    console.log(decode(lineString))
+}
+router.get('/getRoute', function(req, res){
 
-
-    var url = 'https://router.hereapi.com/v8/routes?apiKey=bi9WgKRmGyHx9-CGLiQRAo1bc6UXdklzYn0BWUgUpF8';
+    var url = 'https://router.hereapi.com/v8/routes?';
+    url += 'apiKey='+'QZPcQ4vAyFdtMD9sibX5TY8VPUetjq0aU9EgM3MhjDg'
     url += '&transportMode='+req.query.mode
     url += '&origin='+req.query.latOrigin+","+req.query.lngOrigin
-    url += '&destination='+req.query.latDestination+bi9WgKRmGyHx9-CGLiQRAo1bc6UXdklzYn0BWUgUpF8","+req.query.lngDestination
-    url += '&return=summary'
-    console.log(url)
+    url += '&destination='+req.query.latDestination+','+req.query.lngDestination
+    url += '&return=polyline'
     // &transportMode=car&origin=52.5308,13.3847&destination=52.5323,13.3789&return=summary';
 
 
     fetch(url, {
         method: 'GET',
     }).then(response => (response.json()))
-    .then(data => res.send(data));
-//     fetch(url)
-//   .then(response => (response.json()))
-//   .then(data => res.send(data));
+    .then(data => {
+        printRuta(data);
+        res.send(data);
+    });
 })
 
 app.use(router)
